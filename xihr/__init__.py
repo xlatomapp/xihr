48ed7dfe78e51dd05dcafe555a93b885cd35a9e6
xliu <xlatom1009@gmail.com>
2025-09-27 17:53:12 +0900

    tmp commit

--- xihr/__init__.py
+++ xihr/__init__.py
@@ -5,6 +5,6 @@ __email__ = "xlatom1009@gmail.com"
 __version__ = "0.1.0"
 
 from .config import Config
-from .core import PCKeibaDataFeeder
 
-all = ["Config", "PCKeibaDataFeeder"]
+
+all = ["Config"]
--- xihr/core/datafeed/dataloader/transform/__init__.py
+++
@@ -1,4 +0,0 @@
-from .entires import *
-from .payoff import *
-from .race import *
-from .transformation import *
